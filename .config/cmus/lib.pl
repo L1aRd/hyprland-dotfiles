71e344758f19e98101f6433e99b7f0bd31a28cbe
L1aRd <danafedotov844@gmail.com>
2024-12-22 14:32:21 +0300

    dotfiles

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,11 @@
+/home/daniil/Music/Jack White 2014 - Lazaretto/01. Three Women.mp3
+/home/daniil/Music/Jack White 2014 - Lazaretto/02. Lazaretto.mp3
+/home/daniil/Music/Jack White 2014 - Lazaretto/03. Temporary Ground.mp3
+/home/daniil/Music/Jack White 2014 - Lazaretto/04. Would You Fight For My Love_.mp3
+/home/daniil/Music/Jack White 2014 - Lazaretto/05. High Ball Stepper.mp3
+/home/daniil/Music/Jack White 2014 - Lazaretto/06. Just One Drink.mp3
+/home/daniil/Music/Jack White 2014 - Lazaretto/07. Alone In My Home.mp3
+/home/daniil/Music/Jack White 2014 - Lazaretto/08. Entitlement.mp3
+/home/daniil/Music/Jack White 2014 - Lazaretto/09. That Black Bat Licorice.mp3
+/home/daniil/Music/Jack White 2014 - Lazaretto/10. I Think I Found The Culprit.mp3
+/home/daniil/Music/Jack White 2014 - Lazaretto/11. Want And Able.mp3
